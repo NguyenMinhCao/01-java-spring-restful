2145cb01289cb54619489a3dae12a2e3cce59009
NguyenMinhCao <caonguyenlaptrinh@gmail.com>
2024-12-27 14:34:29 +0700

    u

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -3,7 +3,6 @@ package vn.hoidanit.jobhunter.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import vn.hoidanit.jobhunter.service.EmailService;
 import vn.hoidanit.jobhunter.service.SubscriberService;
 
 import org.springframework.web.bind.annotation.GetMapping;
